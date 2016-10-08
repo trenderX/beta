6fe10ea33d78754a2b4c97825bdbc5f0422a3200
Richard Morales <moralesrichard@live.com>
2016-10-08 12:15:08 -0500

    Wrote small function to seed the DB with dummy data.

--- server/routes/tagsRouter.js
+++ server/routes/tagsRouter.js
@@ -4,90 +4,94 @@ var router = express.Router();
 //db models
 var Tag = require('../db/models/tags.model')
 var TagService = require('../http-client/tags.service')
-var addTag = Tag({
-  text: {
-    value: 'Javascript',
-    unique: true
-  },
-  context: {
-    main: 'Code',
-    sub: false,
-    subType: null,
-  },
-  count: 0,
-  // tabs: {
-  //   first: 'social',
-  //   second: 'media',
-  //   third: 'places',
-  //   fourth: 'shop',
-  //   fifth: 'events',
-  //   sixth: 'web',
-  //   seventh: 'apps',
-  //   eight: 'movies/books',
-  //   ninth: 'jobs'
-  // }
-});
 
-addTag.save(function() {
-  console.log('Tag created!');
-});
 
-var addTwo = Tag({
-  text: {
-    value: 'Medici',
-    unique: true
-  },
-  context: {
-    main: 'Coffee',
-    sub: false,
-    subType: null,
-  },
-  count: 0,
-  // tabs: {
-  //   first: 'places',
-  //   second: 'social',
-  //   third: 'media',
-  //   fourth: 'shop',
-  //   fifth: 'events',
-  //   sixth: 'web',
-  //   seventh: 'apps',
-  //   eight: 'movies/books',
-  //   ninth: 'jobs'
-  // }
-});
+function seedDB() {
+  // dummy tags, move to a diff file.
+  var addTag = {
+    text: {
+      value: 'Javascript',
+      unique: true
+    },
+    context: {
+      main: 'Code',
+      sub: false,
+      subType: null,
+    },
+    count: 0,
+    // tabs: {
+    //   first: 'social',
+    //   second: 'media',
+    //   third: 'places',
+    //   fourth: 'shop',
+    //   fifth: 'events',
+    //   sixth: 'web',
+    //   seventh: 'apps',
+    //   eight: 'movies/books',
+    //   ninth: 'jobs'
+    // }
+  };
+  var addTwo = {
+    text: {
+      value: 'Medici',
+      unique: true
+    },
+    context: {
+      main: 'Coffee',
+      sub: false,
+      subType: null,
+    },
+    count: 0,
+    // tabs: {
+    //   first: 'places',
+    //   second: 'social',
+    //   third: 'media',
+    //   fourth: 'shop',
+    //   fifth: 'events',
+    //   sixth: 'web',
+    //   seventh: 'apps',
+    //   eight: 'movies/books',
+    //   ninth: 'jobs'
+    // }
+  };
+  var addThree = {
+    text: {
+      value: 'iWatch',
+      unique: true
+    },
+    context: {
+      main: 'Apple',
+      sub: true,
+      subType: 'Smartwatch',
+    },
+    count: 0,
+    // tabs: {
+    //   first: 'apps',
+    //   second: 'social',
+    //   third: 'media',
+    //   fourth: 'shop',
+    //   fifth: 'events',
+    //   sixth: 'web',
+    //   seventh: 'places',
+    //   eight: 'movies/books',
+    //   ninth: 'jobs'
+    // }
+  };
 
-addTwo.save(function() {
-  console.log('second created!');
-});
+  var query = [addTag, addTwo, addThree],
+    update = { expire: new Date() },
+    options = { upsert: true, new: true, setDefaultsOnInsert: true };
 
-var addThree = Tag({
-  text: {
-    value: 'iWatch',
-    unique: true
-  },
-  context: {
-    main: 'Apple',
-    sub: true,
-    subType: 'Smartwatch',
-  },
-  count: 0,
-  // tabs: {
-  //   first: 'apps',
-  //   second: 'social',
-  //   third: 'media',
-  //   fourth: 'shop',
-  //   fifth: 'events',
-  //   sixth: 'web',
-  //   seventh: 'places',
-  //   eight: 'movies/books',
-  //   ninth: 'jobs'
-  // }
-});
+  query.map(function(tag) {
+    // this function will check if tag exist
+    // and addOne if not.
+    Tag.findOneAndUpdate(tag, update, options, function(error, result) {
+      if (error) return;
+    });
+  })
+}
 
-addThree.save(function() {
-  console.log('third created!');
-});
-
+seedDB();
 
 // Add new tags if not already exists
 router.post('/', function(req, res) {
